5b7308c98cc45df4c28cf0dba64f8191b411c28c
laushinka <laurie.malau@gmail.com>
2016-07-04 21:25:12 -0400

    One test left

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,13 @@
 class PostsController < ApplicationController
+  def new
+    # @post = Post.create
+  end
 
+  def create
+    # Grab the params
+    @post = Post.create(params[:id])
+    # Passing the @post instance variable through the route
+    # so the view can have access to the submitted form params
+    redirect_to new_post_path(post: @post)
+  end
 end
